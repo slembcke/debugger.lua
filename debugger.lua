54f3c3513b28668c97593d339dae216dc621ec01
slembcke <slembcke@gmail.com>
2019-02-25 13:40:35 -0600

    Fixes for #59

--- debugger.lua
+++ debugger.lua
@@ -434,24 +434,22 @@ local stdout_isatty = 1
 local ffi = (jit and require("ffi"))
 if ffi then
 	ffi.cdef[[
-		#if defined(__linux__)
-			#include <unistd.h>
-			#define ISATTY(FD) isatty(FD)
-		#elif defined(__APPLE__)
-			#include <unistd.h>
-			#define ISATTY(FD) isatty(FD)
-		#elif defined(_WIN32)
-			#include <Windows.h>
-			#define ISATTY(FD) _isatty(FD)
-		#endif
+		int isatty(int); // Unix
+		int _isatty(int); // Windows
 		void free(void *ptr);
 		
 		char *readline(const char *);
 		int add_history(const char *);
 	]]
 	
-	stdin_isatty = ffi.C.ISATTY(0)
-	stdout_isatty = ffi.C.ISATTY(1)
+	local function get_func_or_nil(sym)
+		local success, func = pcall(function() return ffi.C[sym] end)
+		return success and func or nil
+	end
+	
+	local isatty = get_func_or_nil("isatty") or get_func_or_nil("_isatty")
+	stdin_isatty = isatty(0)
+	stdout_isatty = isatty(1)
 end
 
 -- Conditionally enable color support.
